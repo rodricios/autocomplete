2fb3ef9eff3dc8993a33cbedc1f87ea7dbda16a8
rodricios <rodrigopala91@gmail.com>
2015-02-25 12:39:48 -0800

    added python3 support (except clauses), removed pkl file

--- autocomplete/__init__.py
+++ autocomplete/__init__.py
@@ -1,34 +1,32 @@
 import os
 
-from bottle import route, run, request
+import bottle
+
+from bottle import route, run, request, template, view
 
 import models
 
 import autocomplete
 
+from autocomplete import models
+
 """load the classic Norvig big.txt corpus"""
-"""
-print("training!")
-
-if not models.load_models():
-    #print(os.listdir(os.curdir))
-    with open(os.path.join(os.path.dirname(__file__), 'big.txt'),'rb') as f:
-        print(os.listdir(os.curdir))
-        models.train_models(str(f.read()))
-
-print("done training!")
-"""
 
 def run_server(port_num=8080):
-    if not models.load_models():
-        #print(os.listdir(os.curdir))
-        with open(os.path.join(os.path.dirname(__file__), 'big.txt'),'rb') as f:
-            print(os.path.join(os.path.dirname(__file__), 'big.txt'))
-            models.train_models(str(f.read()))
+
+
+    models.load_models()
+    bottle.TEMPLATE_PATH.insert(0, os.path.join(os.path.dirname(__file__),'views'))
+    bottle.debug(True)
 
     @route('/<wordA>/<wordB>')
     def index(wordA, wordB):
         return  dict(autocomplete.predict(wordA, wordB))
+
+    @route('/bigcorpus')
+    @view('bigcorpus')
+    def bigcorpus():
+        return {'suggestions':models.WORDS_MODEL}
 
     run(host='localhost', port=port_num)
 
@@ -37,10 +35,6 @@ if __name__ == "__main__":
     """load the classic Norvig big.txt corpus"""
     print("training!")
 
-    if not models.load_models():
-        #print(os.listdir(os.curdir))
-        with open(os.path.join(os.path.dirname(__file__), 'big.txt'),'rb') as f:
-            print(os.path.join(os.path.dirname(__file__), 'big.txt'))
-            models.train_models(str(f.read()))
+    models.load_models()
 
     print("done training!")
--- autocomplete/models.py
+++ autocomplete/models.py
@@ -19,14 +19,14 @@ WORD_TUPLES_MODEL = {}
 
 #This step is where we transform "raw" data
 # into some sort of probabilistic model(s)
-def train_models(corpus, path=None):
+def train_models(corpus, save_model=True):
     """takes in a preferably long string (corpus), split that string -> list,
     we \"chunk\" (partition) -> list -> list of 2-elem list,
     create dictionary, where each key = first elem;
         each value = Counter([second elems])
 
-    Optionally include a path if you intend on pickle'ing the
-    model (RECOMMENDED!)
+    Will save model by default; set second argument to false if you wish to not
+    save the model.
     """
 
     # "preperation" step
@@ -54,7 +54,17 @@ def train_models(corpus, path=None):
             # hack-y fix for uneven # of elements in WORD_TUPLES
             pass
 
-    save_models(os.path.dirname(__file__))
+    if save_model:
+        save_models(os.path.dirname(__file__))
+
+
+def train_bigtxt():
+    """unnecessary helper function for training against
+    default corpus data (big.txt)"""
+
+    with open(os.path.join(os.path.dirname(__file__), 'big.txt'),'rb') as f:
+        print(os.path.join(os.path.dirname(__file__), 'big.txt'))
+        train_models(str(f.read()))
 
 
 def save_models(path):
@@ -62,13 +72,19 @@ def save_models(path):
     #save for next use. pickle format: (key=model name, value=model)
     pickle.dump({'words_model': WORDS_MODEL,
                 'word_tuples_model': WORD_TUPLES_MODEL},
-                open(os.path.join(path,'models_compressed.pkl'), 'wb'))
+                open(os.path.join(path,'models_compressed.pkl'), 'wb'),
+                protocol=2)
 
 
-def load_models():
-    """try to load models"""
+def load_models(load_path=None):
+    """Load autocomplete's built-in model (uses Norvig's big.txt). Optionally
+    provide the path to Python pickle object."""
+
+    if load_path is None:
+        load_path = os.path.join(os.path.dirname(__file__),
+                    'models_compressed.pkl')
     try:
-        models = pickle.load(open(os.path.join(os.path.dirname(__file__),'models_compressed.pkl'),'rb'))
+        models = pickle.load(open(load_path,'rb'))
 
         global WORDS_MODEL
         WORDS_MODEL = models['words_model']
@@ -77,6 +93,14 @@ def load_models():
         WORD_TUPLES_MODEL = models['word_tuples_model']
 
         print("successfully loaded: models_compressed.pkl")
-        return True
-    except IOError, KeyError:
-        return False
+    except IOError:
+        print("Error in opening pickle object. Training on default corpus text.")
+        train_bigtxt()
+    except KeyError:
+        print("Error in loading both predictve models.\
+              Training on default corpus text.")
+        train_bigtxt()
+    except ValueError:
+        print("Corrupted pickle string.\
+              Training on default corpus text (big.txt)")
+        train_bigtxt()
